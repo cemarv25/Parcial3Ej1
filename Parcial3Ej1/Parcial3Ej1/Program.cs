0803bdbe1d2289cb4dd6f2eb1618c21da7c01cf4
César Adrián Martínez Valdez <A01566625@itesm.mx>
2019-05-02 13:53:14 -0600

    Initial Commit

---
+++ Parcial3Ej1/Parcial3Ej1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ej1P3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    static class AnimalData
+    {
+        public static string Name;
+    }
+
+    class Cat : IInterface
+    {
+
+        string strC = AnimalData.Name;
+        public string Talk(string str)
+        {
+            Sound.Sonido();
+            return str;
+        }
+    }
+
+    class Dog : IInterface
+    {
+        string strD = AnimalData.Name;
+        public string Talk(string str)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    interface IInterface
+    {
+        string Talk(string str);
+    }
+
+    class Sound
+    {
+        public static void Sonido()
+        {
+
+        }
+    }
+}
